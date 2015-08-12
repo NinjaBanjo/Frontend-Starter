c09a0ae2e874df15b33ec9f828c3a52cb4bfa1ae
Ninja Banjo <ninja@ninjabanjo.guru>
2015-08-12 16:05:39 -0500

    switching contentView to a layoutView instead of an itemView

--- js/views/ContentView.js
+++ js/views/ContentView.js
@@ -1,7 +1,7 @@
 import Marionette from 'backbone.marionette';
 import MarionetteHelpers from '../helpers/MarionetteHelpers';
 
-class ContentView extends Marionette.ItemView {
+class ContentView extends Marionette.LayoutView {
   constructor(...rest) {
     super(...rest)
     this.templateHelpers = MarionetteHelpers.templateDataHelper
